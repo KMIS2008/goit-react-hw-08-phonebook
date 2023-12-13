20b196432655bd470d229177a7fc71d26f365ca9
KMIS2008 <lion.irisha@gmail.com>
2023-12-13 21:05:23 +0200

    finaly5

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,20 +3,14 @@ import {Contater} from './App.styled';
 import {useEffect } from 'react';
 import { useDispatch} from 'react-redux';
 import { Route, Routes } from 'react-router-dom';
-import { lazy } from 'react';
 
-// import AppLayout from './AppLayout/AppLayout';
-// import Home from 'Pages/Home/Home';
-// import Reistr from 'Pages/Registr/Registr';
-// import Login from 'Pages/Login/Login';
-// import ContactsPage from 'Pages/ContactsPage/ContactsPage';
-
+import AppLayout from './AppLayout/AppLayout';
 import { useAuth } from 'redux/hook/useAuth';
 import { refreshUser } from 'redux/auth/operations';
 import { RestrictedRoute } from './RestrictedRoute'; 
 import { PrivateRoute } from './PrivateRoute';
 
-const AppLayout = lazy(() => import('./AppLayout/AppLayout'));
+import { lazy } from 'react';
 const Home = lazy(()=> import('Pages/Home/Home'));
 const Reistr = lazy(()=> import('Pages/Registr/Registr'));
 const Login = lazy(()=> import ('Pages/Login/Login'));
